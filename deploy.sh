9f512cc1e5560658f53c4bf16d31eb0b46d3f86a
Rub21 <rub2106@gmail.com>
2024-12-12 14:23:03 -0500

    Update deployment for prometheus and grafana

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 set -e
-
-if [ -z "$GRAFANA_ADMINPASSWORD" ]; then
-    export GRAFANA_ADMINPASSWORD="1234"
-fi
-
+export GRAFANA_ADMINPASSWORD="${GRAFANA_ADMINPASSWORD:-1234}"
 export CLUSTER_NAME=$(kubectl config current-context)
-
+export ENVIROMENT="${ENVIROMENT:-staging}"
 kubectl get namespace monitoring || kubectl create namespace monitoring
 
 install_Prometheus() {
     read -p "Are you sure you want to upgrade/install prometheus in CLUSTER \"${CLUSTER_NAME}\"? (y/n): " confirm
     if [[ $confirm == [Yy] ]]; then
-
         # Install prometheus and node exporter
         helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
         helm repo update
-        helm install prometheus prometheus-community/prometheus --namespace monitoring
+        # helm install prometheus prometheus-community/prometheus --namespace monitoring
+        helm upgrade --install prometheus prometheus-community/prometheus \
+        --namespace monitoring \
+        --set server.persistentVolume.enabled=true \
+        --set server.persistentVolume.size=10Gi \
+        --set server.persistentVolume.storageClass="gp2"
+        ## Install ingress for prometheus
         kubectl get pods -n monitoring
-        kubectl apply -f ingress/staging-prometheus-ingress.yml --namespace monitoring
+        kubectl apply -f ingress/${ENVIROMENT}-prometheus-ingress.yml --namespace monitoring
         
         # Install grafana
         helm repo add grafana https://grafana.github.io/helm-charts
         helm repo update
-        helm install grafana grafana/grafana --namespace monitoring --set-string adminPassword=$GRAFANA_ADMINPASSWORD
+        # helm install grafana grafana/grafana --namespace monitoring --set-string adminPassword=$GRAFANA_ADMINPASSWORD
+        helm upgrade --install grafana grafana/grafana \
+        --namespace monitoring \
+        --set persistence.enabled=true \
+        --set persistence.size=10Gi \
+        --set persistence.storageClass="standard" \
+        --set server.nodeSelector."nodegroup_type"="web_large"
+        ## Install ingress for Grfana
         kubectl get pods -n monitoring | grep grafana
-        kubectl apply -f ingress/staging-grafana-ingress.yml --namespace monitoring
+        kubectl apply -f ingress/${ENVIROMENT}-grafana-ingress.yml --namespace monitoring
     fi
 }
 
